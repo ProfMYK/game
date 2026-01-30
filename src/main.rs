25cf49f14ab68365730a8a884eb545cb223c8aa0
Fortran <mykomurcu1@gmail.com>
2026-01-31 00:31:03 +0300

    This is a game project, probabbly not gonna finish it though

---
+++ src/main.rs
@@ -0,0 +1,185 @@
+use std::collections::HashMap;
+
+use raylib::prelude::*;
+
+struct SpriteAnimation {
+    texture: Texture2D,
+    frame_width: f32,
+    num_frames: u32,
+    current_frame: u32,
+    frames_counter: u32,
+    anim_speed: u32, // REVERSED
+}
+
+impl SpriteAnimation {
+    fn new(sprite: Texture2D, num_frames: u32, speed: u32) -> SpriteAnimation {
+        let frame_width = sprite.width as f32 / num_frames as f32;
+        SpriteAnimation { 
+            texture: sprite, 
+            frame_width,
+            num_frames, 
+            current_frame: 0, 
+            frames_counter: 0, 
+            anim_speed: speed, 
+        }
+    }
+
+    fn animate(&mut self) {
+        self.frames_counter += 1;
+        if self.frames_counter >= (60 / self.anim_speed) {
+            self.frames_counter = 0;
+            self.current_frame += 1;
+
+            if self.current_frame > self.num_frames - 1 {
+                self.current_frame = 0;
+            }
+        }
+    }
+
+    fn draw(&self, pos: Vector2, d: &mut RaylibDrawHandle) {
+        let source_rec = Rectangle::new(
+            self.current_frame as f32 * self.frame_width, 
+            0.0, 
+            self.frame_width, 
+            self.texture.height as f32
+        );
+
+        let dest_rec = Rectangle::new(pos.x, pos.y, self.frame_width, self.texture.height as f32);
+        d.draw_texture_pro(
+            &self.texture,
+            source_rec,
+            dest_rec,
+            Vector2::new(0.0, 0.0), // Origin (for rotation/scaling)
+            0.0,                    // Rotation
+            Color::WHITE,
+        );
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Debug)]
+enum AnimationType {
+    Idle(Direction),
+}
+
+#[derive(PartialEq, Eq, Hash, Debug)]
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
+struct Player {
+    collision: Rectangle,
+    animations: HashMap<AnimationType, SpriteAnimation>,
+    pos: Vector2,
+    current_animation: AnimationType,
+    direction: Direction,
+    is_moving: bool,
+}
+
+impl Player {
+    fn new(x: f32, y: f32, width: f32, height: f32) -> Player {
+        let animations = HashMap::new();
+        Player { 
+            collision: Rectangle::new(x, y, width, height), 
+            animations, 
+            pos: Vector2::zero(), 
+            current_animation: AnimationType::Idle(Direction::DOWN),
+            direction: Direction::DOWN,
+            is_moving: false,
+        }
+    }
+
+    fn add_animation(&mut self, 
+        rl: &mut RaylibHandle, thread: &RaylibThread, 
+        animation_type: AnimationType, 
+        file: &str,
+        num_frames: u32, speed: u32)
+    {
+        let sprite = rl.load_texture(&thread, file).unwrap();
+        let animation = SpriteAnimation::new(sprite, num_frames, speed);
+        self.animations.insert(animation_type, animation);
+    }
+
+    fn change_animation(&mut self, animation_type: AnimationType) {
+        self.current_animation = animation_type;
+    }
+
+    fn animate(&mut self) {
+        let animation = self.animations.get_mut(&self.current_animation)
+            .expect("Couldn't found animation {:?}, on player.");
+        animation.animate();
+    }
+
+    fn draw(&self, d: &mut RaylibDrawHandle) {
+        let animation = self.animations.get(&self.current_animation)
+            .expect("Couldn't found animation {:?}, on player.");
+        animation.draw(self.pos, d);
+    }
+
+    fn change_direction(&mut self, dir: Direction) {
+        self.change_animation(AnimationType::Idle(dir));
+    }
+}
+
+fn main() {
+    let w = 640;
+    let h = 480;
+
+    let (mut rl, thread) = raylib::init()
+        .size(w, h)
+        .title("Non-Hot Reloaded Game")
+        .build();
+
+
+    let mut player = Player::new(42.0, 58.0, 12.0, 28.0);
+    player.add_animation(
+        &mut rl, &thread, 
+        AnimationType::Idle(Direction::DOWN), "resources/Hero/Sprites/IDLE/idle_down.png",
+        8, 20
+    );
+
+    player.add_animation(
+        &mut rl, &thread, 
+        AnimationType::Idle(Direction::UP), "resources/Hero/Sprites/IDLE/idle_up.png",
+        8, 20
+    );
+
+    player.add_animation(
+        &mut rl, &thread, 
+        AnimationType::Idle(Direction::RIGHT), "resources/Hero/Sprites/IDLE/idle_right.png",
+        8, 20
+    );
+
+    player.add_animation(
+        &mut rl, &thread, 
+        AnimationType::Idle(Direction::LEFT), "resources/Hero/Sprites/IDLE/idle_left.png",
+        8, 20
+    );
+
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        if rl.is_key_pressed(KeyboardKey::KEY_A) {
+            player.change_direction(Direction::LEFT);
+        }
+        if rl.is_key_pressed(KeyboardKey::KEY_D) {
+            player.change_direction(Direction::RIGHT);
+        }
+        if rl.is_key_pressed(KeyboardKey::KEY_S) {
+            player.change_direction(Direction::DOWN); 
+        }
+        if rl.is_key_pressed(KeyboardKey::KEY_W) {
+            player.change_direction(Direction::UP);
+        }
+
+        player.animate();
+
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::get_color(0x181818FF));
+
+        player.draw(&mut d);
+    }
+}
